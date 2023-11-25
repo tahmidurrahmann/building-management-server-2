d27f35c5bec5fe162263c5049f01eaf558c97b6b
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-11-25 17:44:11 +0600

    added the apartment collection

--- index.js
+++ index.js
@@ -22,14 +22,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const apartmentCollection = client.db("buildMinder").collection("apartments");
+
+        app.get("/apartments", async (req, res) => {
+            const result = await apartmentCollection.find().toArray();
+            res.send(result);
+        })
+        
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+        
     }
 }
 run().catch(console.dir);
